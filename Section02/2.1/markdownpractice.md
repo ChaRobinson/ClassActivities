a28ec7bc8bc1df0e3c3fe7858e4675161bf1af66
lilbigangel86 <lilbigangel86@yahoo.com>
2023-01-17 22:38:24 -0500

    Section 2 up to 2.2.2.

---
+++ Section02/2.1/markdownpractice.md
@@ -0,0 +1,19 @@
+# Top Level Heading #
+## Hash Tag Second Heading ##
+### **Third heading look at me I'm coding** ###
+
+- Food
+- Groceries
+- Fuel
+- Sleep
+
+1. What
+2. When
+3. Where
+4. How
+
+**Charity R**
+
+_Charity R_
+
+~~ I don't like peanut butter and jelly ~~
---
+++ Section02/2.2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,27 @@
+// create a constant variable (const)
+const string = "Hello";
+// create a variable that can be reassigned (let)
+let months;
+// create three variables and assign them values of different data types (=)
+let year = 2002;
+let month = "February";
+let leap = true;
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(typeof year);
+console.log(typeof month);
+console.log(typeof leap);
+// change the value referenced by a variable (dynamic typing)
+year = "two";
+
+// print the type of the variable you just changed (typeof)
+console.log(typeof year);
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+let name = "Charity";
+let language = "English";
+let number = 2080;
+let expert = true;
+
+// What type of data needs to go in each variable?
+console.log("Hello, my name is " + name + ", and I am learning " + language + ". I have been practicing for only " +  number + " weeks, so my status as a master is: " + expert + ".");
