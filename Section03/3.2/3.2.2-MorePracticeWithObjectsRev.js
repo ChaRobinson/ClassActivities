3834226451a9484033ee80e3603767d0acfba54a
lilbigangel86 <lilbigangel86@yahoo.com>
2023-01-31 20:38:05 -0500

    html and CSS work

---
+++ Section03/3.2/3.2.2-MorePracticeWithObjectsRev.js
@@ -0,0 +1,54 @@
+/*
+  3.2.2-MorePracticeWithObjects.js
+  ========================================
+*/
+
+// bracket and dot notation
+//-------------------------
+const key = '1';
+const obj = {
+  key: "The key is key",
+  one: "The key is 1"
+};
+
+// what will the following lines print?
+console.log(obj["key"]);
+console.log(obj[key]);
+/*
+Create an object with at least four properties, each with a different data type
+Name one of the four properties "collection" and set its value to an Array or Object
+*/
+
+
+// access a value in in "collection" property
+
+
+// Nested Arrays and Objects
+//===========================
+
+// below is an array of strings with 5 elements
+const newObj = ["key", "1", "method", "favorite", "list"];
+// below is an object with 2x key-value pairs
+const newObj = {
+  key: "the key is 'key'"
+}
+// adding a method to the object `newObj`
+
+
+// add a nested object key-value pair to the object `newObj`
+// fill in your favorite movie and color below
+
+
+// add a complex array to the object `newObj`
+
+
+/*
+How many lines will the following for...of statement print?
+What do you expect to see on each line?
+*/
+
+
+// use a template literal to print a sentence about your favorite movie and color
+
+// access the values "b", 4, and 6 from obj.list
+
---
+++ Section03/3.3/3.3.1/index.js
@@ -0,0 +1,8 @@
+console.log('This came from index.js');
+function kite(kites) {
+  console.log(kites);
+};
+let kites = "fly high";
+console.log(kites);
+kite();
+
